e608311f6f492673cc6fcf5914f946addf29b33b
guanxu <xm@sian.com>
2019-10-09 15:39:05 +0800

    详情

--- src/js/xiangqing.js
+++ src/js/xiangqing.js
@@ -46,3 +46,34 @@ $(function () {
         }
     })
 })
+// 懒加载
+lazyRender();
+
+$(window).on('scroll', function () {
+    console.log('运行了一次');
+    lazyRender();
+});
+
+function lazyRender() {
+    $('img').each(function () {
+        if (checkShow($(this)) && !isLoaded($(this))) {
+            loadImg($(this));
+        }
+    })
+}
+function checkShow($img) { // 传入一个img的jq对象 
+    var scrollTop = $(window).scrollTop(); //即页面向上滚动的距离 
+    var windowHeight = $(window).height(); // 浏览器自身的高度
+    var offsetTop = $img.offset().top; //目标标签img相对于document顶部的位置 
+    if (offsetTop < (scrollTop + windowHeight) && offsetTop > scrollTop) { //在2个临界状态之间的就为出现在视野中的 
+        return true;
+    }
+    return false;
+}
+function isLoaded($img) {
+    return $img.attr('data-src') === $img.attr('src'); //如果data-src和src相等那么就是已经加载过了 
+}
+function loadImg($img) {
+    $img.attr('src', $img.attr('data-src')); // 加载就是把自定义属性中存放的真实的src地址赋给src属性 
+}
+//懒加载结束
