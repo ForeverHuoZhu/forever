69835d790d0897a73d27f2f5b0e1bc605fec0915
lxl <xm@sina.com>
2019-10-12 14:24:00 +0800

    lxl

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 var body = document.getElementsByTagName("body")[0];
 var cCount = 0;
-var clickArr = [1428, 2488, 3568, 4620, 5666, 6748, 7920, 8860];
-var scrollArr = [1424, 2340, 3528, 4580, 5633, 6734, 7920, 8834];
+var clickArr = [1428, 2488, 3568, 4620, 5666, 6748, 7792, 8860];
+var scrollArr = [1424, 2340, 3528, 4580, 5633, 6734, 7792, 8834];
 var leftli = document.querySelectorAll("#left>ul>li");
 var place = document.getElementById("place");
 var province = document.getElementById("province");
@@ -35,6 +35,7 @@ window.onload = window.onresize = function() {
 
         window.onscroll = function() {
             var scrolltop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+            console.log(scrolltop);
             if (scrolltop >= 623) {
                 left.style.display = "block";
             } else {
@@ -302,4 +303,4 @@ $(function() {
 (function() {
     var goods = getCookie("goods") ? JSON.parse(getCookie("goods")) : [];
     $(".spnum").text(goods.length);
-}())
+}());
