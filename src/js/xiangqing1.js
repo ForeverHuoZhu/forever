eb05ff99401af6aaabc583ed25669eef87c23da3
guanxu <xm@sian.com>
2019-10-12 18:35:36 +0800

    详情

---
+++ src/js/xiangqing1.js
@@ -0,0 +1,45 @@
+$(function () {
+    $(".other-infoCoding").text("商品编码：ZA8124910")
+    $(".pib-title-class").text("左岸")
+    $(".pib-title-detail").text("2020男款圆领长袖T大字母印花休闲时尚男士长袖T恤")
+    $(".goods-description-title").text("男款圆领长袖T恤 舒适微弹 大字母印花")
+    $(".goodsprice").html(`59`)
+    $(".pbox-off").text("3折")
+    $(".pbox-market>del").text("198")
+    $(".size-ullist-item").html(`
+                                <li class="size-list-item">165
+                                    <span class="i-select" style="display:none"></span>
+                                </li>
+                                <li class="size-list-item">170
+                                    <span class="i-select" style="display:none"></span>
+                                </li>
+                                <li class="size-list-item">175
+                                    <span class="i-select" style="display:none"></span>
+                                </li>
+                                <li class="size-list-item">180
+                                    <span class="i-select" style="display:none"></span>
+                                </li>
+                                <li class="size-list-item">185
+                                    <span class="i-select" style="display:none"></span>
+                                </li>
+                                <li class="size-list-item">190
+                                    <span class="i-select" style="display:none"></span>
+                                </li>
+                                <div class="remind-ullist" style="display: none;">请选择尺码</div>`)
+    $(".dc-images-con").html(
+        `<img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs1.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs2.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs3.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs4.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs5.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs6.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs7.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs8.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs9.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs10.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs11.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs12.jpg" alt="">
+        <img src="../images/xiangqing1/ljz.gif" data-src="../images/xiangqing1/zs13.jpg" alt="">`
+    )
+
+})
